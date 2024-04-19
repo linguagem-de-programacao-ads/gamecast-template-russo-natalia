bbc9435dca7079c051126542c8892ad1e8181955
russo-natalia <natalia.russo@sptech.school>
2024-04-18 21:24:01 -0300

    Aula 3 do BootCamp

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,4 @@
 async function buscar() {
-    console.log("Passei por aqui");
 
     const resposta = await fetch("https://660f44ad356b87a55c510d10.mockapi.io/agendas");
     const respostaDadosAgenda = await resposta.json();
@@ -9,13 +8,34 @@ async function buscar() {
     const cards = document.getElementById("cards_games");
 
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-        return "teste "
-    });
-}
+        return `
+        <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome}</strong> </p>
+                    <p> ${itemAgenda.descricao} </p>
+                    <p><strong>Gamers: </strong></p>
 
-console.log("antes de buscar")
+                    <div class="gamers">
+                    ${
+                        itemAgenda.gamers.map((gamerItem) => {
+                        return `<div class="gamerItem">@${gamerItem}</div>`
+                        }).join('')
+                    }
+                    </div>
+
+                  
+                </div>  
+                <div class="assistir">
+                        <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+        `
+    }).join('');
+}
 
 
 buscar();
-
-console.log("depois de buscar")
